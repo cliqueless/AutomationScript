e17773e85887a6ce692f84195a8faefb6c331e8a
Tim Scott <tim@asgllc.com>
2023-05-16 15:03:53 -0400

    Add project files.

---
+++ last_time_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("last_time_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("last_time_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("B8F823A6-982D-4C53-9CA5-6D98AB55D620")]
+[assembly: AssemblyVersion("1.0.0.0")]
